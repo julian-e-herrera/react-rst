dfd087851cf37b32e731505dc6393d54698415b2
julian-e-herrera <je.herrera@globant.com>
2020-10-13 13:40:22 -0300

    prettier

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,20 +1,21 @@
-import React from 'react';
+import React from 'react'
 //import logo from './logo.svg';
-import './App.css';
-import { Card } from './components/Card/box';//card
-import { PopupCard } from './components/Card/popupCard';//card
-import { Form } from './components/form';
-import { Footer } from './components/footer';
+import './App.css'
+import { Card } from './components/Card/box' //card
+import { PopupCard } from './components/Card/popupCard' //card
+import { Form } from './components/form'
+import { Footer } from './components/footer'
 //import { Connection } from './components/api';
 //import { CardList } from './components/cardList';
-import  Carousel  from 'react-elastic-carousel';
+import Carousel from 'react-elastic-carousel'
 
-
- const accesKey = 'rQ-_SmO3ayd_uvDdmk1atdqJifxUQahY2IdZM90ux6k';
- const endPoint = 'https://api.unsplash.com/search/photos';
- const breakpoint =[
-  {width :500 ,itemsToShow : 1}, {width : 720 ,itemsToShow : 2},
-   {width : 1200 ,itemsToShow : 3}, {width : 1500 ,itemsToShow : 4}
+const accesKey = 'rQ-_SmO3ayd_uvDdmk1atdqJifxUQahY2IdZM90ux6k'
+const endPoint = 'https://api.unsplash.com/search/photos'
+const breakpoint = [
+  { width: 500, itemsToShow: 1 },
+  { width: 720, itemsToShow: 2 },
+  { width: 1200, itemsToShow: 3 },
+  { width: 1500, itemsToShow: 4 },
 ]
 
 export class App extends React.Component {
@@ -24,77 +25,74 @@ export class App extends React.Component {
       error: null,
       isLoaded: false,
       abierto: false,
-      id : 1 ,
-      items: []
-      
-    };
-    
+      id: 1,
+      items: [],
+    }
   }
 
   componentDidMount() {
-    fetch(endPoint + '?query=new-york&client_id=' + accesKey) 
-      .then(res => res.json())
+    fetch(endPoint + '?query=new-york&client_id=' + accesKey)
+      .then((res) => res.json())
       .then(
         (result) => {
-            console.log(result)
+          console.log(result)
           this.setState({
             isLoaded: true,
-            items: result.results
-          });console.log(this.state.items)
+            items: result.results,
+          })
+          console.log(this.state.items)
         },
         (error) => {
           this.setState({
             isLoaded: true,
-            error
-          });
+            error,
+          })
         }
       )
   }
 
-  abrirPopup = (param,ide) => {
-     this.setState({
-       abierto: param,
-       id:ide
-      })
-   }
-  
-
-render (){
-  
-  let className = "overlay";
-  const { error, isLoaded } = this.state;
-  if (error) {
-    return <div>Error: {error.message}</div>;
-  } else if (!isLoaded) {
-    return <div>Loading...;)</div>;
+  abrirPopup = (param, ide) => {
+    this.setState({
+      abierto: param,
+      id: ide,
+    })
   }
-  if(this.state.abierto){
-    className += ' active';
-         return (  
-         <PopupCard  props={this.state.items.filter(card => card.id === this.state.id )}
-          handler={this.abrirPopup}/>
-         )
+
+  render() {
+    let className = 'overlay'
+    const { error, isLoaded } = this.state
+    if (error) {
+      return <div>Error: {error.message}</div>
+    } else if (!isLoaded) {
+      return <div>Loading...;)</div>
     }
-  else {
-        return (
-           <div className="App">
-             <header className="App-header">
-                 <h1>Real Estate Test</h1>  
-             </header>
-             <Form/>
-             <Carousel breakPoints={breakpoint}>
-               {this.state.items.map(item => <Card key={item.id} {...item} handler={this.abrirPopup}/>)}
-               </Carousel>
-             <Footer/>
-             {/* <div className ='box'>
+    if (this.state.abierto) {
+      className += ' active'
+      return (
+        <PopupCard props={this.state.items.filter((card) => card.id === this.state.id)} handler={this.abrirPopup} />
+      )
+    } else {
+      return (
+        <div className="App">
+          <header className="App-header">
+            <h1>Real Estate Test</h1>
+          </header>
+          <Form />
+          <Carousel breakPoints={breakpoint}>
+            {this.state.items.map((item) => (
+              <Card key={item.id} {...item} handler={this.abrirPopup} />
+            ))}
+          </Carousel>
+          <Footer />
+          {/* <div className ='box'>
                <Card/>
                <Card/>
                <Card/>
              </div>  */}
-               {/* <CardList items={this.state.items}/> */}
-           </div>
-        );
-     }
- }
+          {/* <CardList items={this.state.items}/> */}
+        </div>
+      )
+    }
+  }
 }
-export default App;
+export default App
--- my-app/src/components/Card/box.js
+++ my-app/src/components/Card/box.js
@@ -1,55 +1,51 @@
-import React from 'react';
-import { Photo } from '../photo';
-import { Popup } from '../popup';
-import { App } from '../../App';
+import React from 'react'
+import { Photo } from '../photo'
+import { Popup } from '../popup'
+import { App } from '../../App'
 
-import './style.scss';
-
+import './style.scss'
 
 export class Card extends React.Component {
-    abrirPopup = () => {
-        this.props.handler(true,this.props.id);
-      }
-    
-    render() {
-        
-            const items  = this.props;
-             const sti ={
-                 backgroundImage:`url(${items.urls.raw})`,
-                 backgroundSize: 'cover',
-                 backgroundPosition: 'center',
-        };
-            const  txt ={
-                width:' 60%',
-                padding:' 50px',
-                overflowY : 'hidden'
-            };
-            const pp ={
-                width: '100%',
-                height:' 500px',
-                position: 'relative',
-                display: 'flex',
-                background:' white',
-                
-            }
-                return (
-                <div className='pp' style={pp}>
-                     <div className="img" style={sti} alt={items.alt_description}></div>
-                     <div className="container-text"style={txt}>
-                       <a href ='#' onClick={this.abrirPopup}>
-                        <h1 className='title'>{items.description}</h1></a>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                           Quisquam esse illum exercitationem perferendis accusamus, possimus sed molestiae accusantium necessitatibus neque sit aspernatur</p>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                           Possimus facere, sequi. Beatae recusandae, officiis sapiente amet quod vero est vel.</p>
-                      </div>
-                </div>
-          )
-        }
+  abrirPopup = () => {
+    this.props.handler(true, this.props.id)
+  }
+
+  render() {
+    const items = this.props
+    const sti = {
+      backgroundImage: `url(${items.urls.raw})`,
+      backgroundSize: 'cover',
+      backgroundPosition: 'center',
+    }
+    const txt = {
+      width: ' 60%',
+      padding: ' 50px',
+      overflowY: 'hidden',
+    }
+    const pp = {
+      width: '100%',
+      height: ' 500px',
+      position: 'relative',
+      display: 'flex',
+      background: ' white',
+    }
+    return (
+      <div className="pp" style={pp}>
+        <div className="img" style={sti} alt={items.alt_description}></div>
+        <div className="container-text" style={txt}>
+          <a href="#" onClick={this.abrirPopup}>
+            <h1 className="title">{items.description}</h1>
+          </a>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam esse illum exercitationem perferendis
+            accusamus, possimus sed molestiae accusantium necessitatibus neque sit aspernatur
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus facere, sequi. Beatae recusandae,
+            officiis sapiente amet quod vero est vel.
+          </p>
+        </div>
+      </div>
+    )
+  }
 }
-
-
-
-
-
-
