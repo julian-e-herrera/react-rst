5d6ec443ace6df87258875152d20c5038e0b1fd5
julian-e-herrera <43296485+julian-e-herrera@users.noreply.github.com>
2020-11-26 19:55:26 -0300

    try to test

--- redux/src/__tests__/App.test.js
+++ redux/src/__tests__/App.test.js
@@ -2,30 +2,30 @@ import { render, screen } from '@testing-library/react'
 import App from '../App'
 import React from 'react'
 import { estateAxios } from '../config/axios'
-//import { estate } from '../__mocks__/estate'
+import { estate } from '../__mocks__/estate'
 import store from '../store'
 import { Provider } from 'react-redux'
 
 const mockAxios = estateAxios
 test('main app<App/>', () => {})
-// test('main call app<App/>', async () => {
-//   mockAxios.get = jest.fn().mockResolvedValue({
-//     data: estate,
-//   })
-//   render(
-//     <Provider store={store}>
-//       <App />
-//     </Provider>
-//   )
-//   const list = screen.findAllByTestId('property')
-//   expect(await list).toHaveLength(10)
+test('main call app<App/>', async () => {
+  mockAxios.get = jest.fn().mockResolvedValue({
+    data: estate,
+  })
+  render(
+    <Provider store={store}>
+      <App />
+    </Provider>
+  )
+  const list = screen.findAllByTestId('property')
+  expect(await list).toHaveLength(10)
 
-//   expect(mockAxios.get).toHaveBeenCalled()
-//   expect(mockAxios.get).toHaveBeenCalledTimes(1)
-// })
-// test('main text app<App/>', () => {
-//   render(<App />)
-//   const linkElement = screen.getByText(/Real Estate Test/)
-//   expect(linkElement).toBeInTheDocument()
-//   expect(screen.getByTestId('title').tagName).toBe('H1')
-// })
+  expect(mockAxios.get).toHaveBeenCalled()
+  expect(mockAxios.get).toHaveBeenCalledTimes(1)
+})
+test('main text app<App/>', () => {
+  render(<App />)
+  const linkElement = screen.getByText(/Real Estate Test/)
+  expect(linkElement).toBeInTheDocument()
+  expect(screen.getByTestId('title').tagName).toBe('H1')
+})
--- redux/src/__tests__/Login.test.js
+++ redux/src/__tests__/Login.test.js
@@ -1,43 +1,43 @@
-// import React from 'react'
-// import { render, screen } from '@testing-library/react'
-// import { clientAxios } from '../config/axios'
-// import Login from '../components/login/login'
-// import userEvent from '@testing-library/user-event'
-// import { Provider } from 'react-redux'
-// import store from '../store'
-// import { users } from '../__mocks__/user'
+import React from 'react'
+import { render, screen } from '@testing-library/react'
+import { clientAxios } from '../config/axios'
+import Login from '../components/login/login'
+import userEvent from '@testing-library/user-event'
+import { Provider } from 'react-redux'
+import store from '../store'
+import { users } from '../__mocks__/user'
 
 test('main app<App/>', () => {})
 
-// const handleSubmit = jest.fn()
-// test('<Login/> login', () => {
-//   render(
-//     <Provider store={store}>
-//       <Login onClick={handleSubmit} />
-//     </Provider>
-//   )
+const handleSubmit = jest.fn()
+test('<Login/> login', () => {
+  render(
+    <Provider store={store}>
+      <Login onClick={handleSubmit} />
+    </Provider>
+  )
 
-//   //  check why repeat
+  //  check why repeat
 
-//   userEvent.type(screen.getByTestId('username'), 'julian')
-//   userEvent.type(screen.getByTestId('password'), 'admin')
+  userEvent.type(screen.getByTestId('username'), 'julian')
+  userEvent.type(screen.getByTestId('password'), 'admin')
 
-//   //const btnSingin = screen.getByTestId('btn-sing-in')
-// })
-// const mockAxios = clientAxios
+  //const btnSingin = screen.getByTestId('btn-sing-in')
+})
+const mockAxios = clientAxios
 
-// test('main call search user<login/>', async () => {
-//   mockAxios.get = jest.fn().mockResolvedValue({
-//     data: users,
-//   })
-//   render(
-//     <Provider store={store}>
-//       <Login onClick={handleSubmit} />
-//     </Provider>
-//   )
-//   //   const list = screen.findAllByTestId('property')
-//   //   expect(await list).toHaveLength(10)
+test('main call search user<login/>', async () => {
+  mockAxios.get = jest.fn().mockResolvedValue({
+    data: users,
+  })
+  render(
+    <Provider store={store}>
+      <Login onClick={handleSubmit} />
+    </Provider>
+  )
+  //   const list = screen.findAllByTestId('property')
+  //   expect(await list).toHaveLength(10)
 
-//   expect(await mockAxios.get).toHaveBeenCalled()
-//   expect(mockAxios.get).toHaveBeenCalledTimes(1)
-// })
+  expect(await mockAxios.get).toHaveBeenCalled()
+  expect(mockAxios.get).toHaveBeenCalledTimes(1)
+})
